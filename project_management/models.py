d01e2b02049317c71dd9655b7b2c486732298275
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-11-19 13:56:21 +0000

    Create Project model and superuser.

--- project_management/models.py
+++ project_management/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
+PROJECT_STATUS = ((0, "Not completed"), (1, "Completed"))
+APPROVAL_STATUS = ((0, "Not approved"), (1, "Approved"))
 
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="projects"
+    )
+    document = CloudinaryField('')
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=PROJECT_STATUS, default=0)
+
+    class Meta:
+        ordering = ['-date_created', 'owner']
+
+    def __str__(self):
+        return self.title
