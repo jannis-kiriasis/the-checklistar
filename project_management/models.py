e054805e11b964f02853773cb5197c457d9601c3
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-11-19 16:24:57 +0000

    Create model to store project approvers

--- project_management/models.py
+++ project_management/models.py
@@ -3,17 +3,19 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
-PROJECT_STATUS = ((0, "Not completed"), (1, "Completed"))
-APPROVAL_STATUS = ((0, "Not approved"), (1, "Approved"))
+PROJECT_STATUS = ((0, 'Not completed'), (1, 'Completed'))
+APPROVAL_STATUS = ((0, 'Not approved'), (1, 'Approved'))
 
 # Create your models here.
+
+# Project model. Contains all the information related to a project
 
 
 class Project(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     owner = models.ForeignKey(
-        User, on_delete=models.PROTECT, related_name="projects"
+        User, on_delete=models.PROTECT, related_name='projects'
     )
     document = CloudinaryField('')
     description = models.TextField()
@@ -25,3 +27,23 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# ProjectApprover Model: contains all the information to the project's approvers
+class ProjectApprover(models.Model):
+    project = models.ForeignKey(
+        Project, on_delete=models.PROTECT, related_name='approvers'
+        )
+    approver_department = models.CharField(max_length=80)
+    approver = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name='approvers'
+    )
+    approval_date = models.DateField(null=True, blank=True)
+    approval_due_by = models.DateField(null=True, blank=True)
+    status = models.IntegerField(choices=APPROVAL_STATUS, default=0)
+
+    class Meta:
+        ordering = ['approval_date', 'project']
+
+    def __str__(self):
+        return f"Project: {self.project} | Approver: {self.approver} | Department: {self.approver_department}"
