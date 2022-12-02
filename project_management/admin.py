d0386510c5bdf44db44612623179ef0994068f9d
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-12-02 10:11:38 +0000

    Add function to export projects and projectApprovals from admin panel

--- project_management/admin.py
+++ project_management/admin.py
@@ -1,4 +1,6 @@
+import csv
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import Project, ProjectApproval, UserProfile, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
@@ -17,18 +19,62 @@ class ProjectAdmin(SummernoteModelAdmin):
     inlines = [
         ApprovalInline
     ]
+    actions = ("export_as_csv",)
+
+    # fuction to export list of projects from admin panel
+    # from https://stackoverflow.com/questions/58921265/django-admin-download-data-as-csv
+
+
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 
 @admin.register(ProjectApproval)
 class ProjectApprovalAdmin(SummernoteModelAdmin):
 
     list_filter = ('project', 'approver')
-    list_display = ('id', 'project', 'approver', 'approved')
-    actions = ['approve_projects']
+    list_display = ('id', 'project', 'project_id', 'approver', 'approved')
+    actions = ['approve_projects', "export_as_csv"]
+
 
     def approve_projects(self, request, queryset):
         queryset.update(approved=True)
 
+
+    # fuction to export list of projects from admin panel
+    # from https://stackoverflow.com/questions/58921265/django-admin-download-data-as-csv
+
+    
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 @admin.register(UserProfile)
 class UserProfileAdmin(SummernoteModelAdmin):
