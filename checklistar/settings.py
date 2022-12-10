8722b3984a36a718c4671af886d2373b1467fb9b
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-12-10 09:56:12 +0000

    Change debug from True to False, add x frame options

--- checklistar/settings.py
+++ checklistar/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'project_management/templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ["the-checklistar.herokuapp.com", "localhost"]
 
